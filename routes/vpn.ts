27d7329ed938063bbb92d0f338fe326e00e8958c
sharull9 <sharullmusicplaylist@gmail.com>
2024-04-06 15:46:09 +0530

    added vpn route for test

---
+++ routes/vpn.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import { prisma } from "../prisma/client";
+const router = Router();
+
+router.get("/", async (req, res) => {
+	const vpn = [
+		{
+			hostname: "",
+			ip: "23.164.240.140",
+			ping: "33",
+			speed: "",
+			country_long: "United States",
+			country_short: "US",
+			configdata_base64: "",
+		},
+		{
+			hostname: "",
+			ip: "23.164.240.140",
+			ping: "33",
+			speed: "",
+			country_long: "United States",
+			country_short: "US",
+			configdata_base64: "",
+		},
+		{
+			hostname: "",
+			ip: "23.164.240.140",
+			ping: "33",
+			speed: "",
+			country_long: "United States",
+			country_short: "US",
+			configdata_base64: "",
+		},
+		{
+			hostname: "",
+			ip: "23.164.240.140",
+			ping: "33",
+			speed: "",
+			country_long: "United States",
+			country_short: "US",
+			configdata_base64: "",
+		},
+	];
+	return res.status(200).json({ ...vpn });
+});
+
+export const vpnRouter = router;
